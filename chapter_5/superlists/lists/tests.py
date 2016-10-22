c632a951f4a604cb9adad8f2421429ebf5f8bb4d
MilesDuronCIMAT <milesduron@uaz.edu.mx>
2016-10-22 18:15:07 -0500

    Major flaw in functional test before refactoring

--- chapter_5/superlists/lists/tests.py
+++ chapter_5/superlists/lists/tests.py
@@ -12,5 +12,19 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
     	request=HttpRequest()
     	response=home_page(request)
-    	expected_html = render_to_string('home.html')
+    	#only on Django 1.9.5 
+    	expected_html = render_to_string('home.html',request=request)
+    	
+
+    def test_home_page_can_save_a_POST_request(self):
+    	request = HttpRequest()
+    	request.method = 'POST'
+    	request.POST['item_text'] = 'A new list item'
+    	response = home_page(request)
+    	self.assertIn('A new list item', response.content.decode())
+    	expected_html = render_to_string(
+    		'home.html',
+    		{'new_item_text':  'A new list item'},
+    		request=request
+    	) 
     	self.assertEqual(response.content.decode(), expected_html)

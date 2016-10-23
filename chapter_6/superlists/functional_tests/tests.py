c789cf02d6fd14ff16918f547de46609a29afde3
MilesDuronCIMAT <milesduron@uaz.edu.mx>
2016-10-22 20:37:34 -0500

    New URL, view and template to display lists

--- chapter_6/superlists/functional_tests/tests.py
+++ chapter_6/superlists/functional_tests/tests.py
@@ -43,7 +43,7 @@ class NewVisitorTest(LiveServerTestCase):
         # and now the page lists "1: Buy peacock feathers" as an item in a
         # to-do list table 
         inputbox.send_keys(Keys.ENTER)
-        eidth_list_url = self.browser.current_url
+        edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
